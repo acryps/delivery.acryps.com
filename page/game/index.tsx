35e8b1f0f73b20b98f1ba0cefaf211ba2a5b994d
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2023-10-10 15:34:10 +0200

    Add canvas scaffolding

--- page/game/index.tsx
+++ page/game/index.tsx
@@ -7,7 +7,16 @@ export class GameComponent extends Component {
 	resolution = 0.0001;
 
 	render() {
-		
+		const mapCanvas = document.createElement('canvas');
+
+		requestAnimationFrame(() => {
+			const size = mapCanvas.width = mapCanvas.height = this.resolution / (this.radius * 2);
+			
+			const context = mapCanvas.getContext('2d');
+			context.arc(size / 2, size / 2, size / 2, 0, Math.PI * 2);
+			context.fillStyle = 'red';
+			context.fill();
+		});
 
 		return <ui-game>
 			{mapCanvas}
