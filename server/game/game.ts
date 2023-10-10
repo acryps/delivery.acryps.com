09cc3009a6a2e529b36b23fc8882efef36c17125
Eggsecuter <adrian.habeler@gmail.com>
2023-10-10 18:09:33 +0200

    Fix deltatime

--- server/game/game.ts
+++ server/game/game.ts
@@ -51,7 +51,7 @@ export class Game {
 
 		while (this.isRunning) {
 			if (Date.now() > lastTick + this.tickMillisecondsInterval) {
-				const deltaTime = Date.now() - lastTick;
+				const deltaTime = (Date.now() - lastTick) / 1000;
 
 				for (const player of this.players) {
 					player.position.latitude += Math.sin(player.moveAngle) * this.playerSpeed * deltaTime;
