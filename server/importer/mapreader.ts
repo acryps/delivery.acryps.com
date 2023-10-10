45305c880cb2cd46c2662ee6f2e3ceb3f8a1fc59
yael <yael.fassbind@acryps.com>
2023-10-10 18:32:28 +0200

    WIP: Add read xml file and parse to json

--- server/importer/mapreader.ts
+++ server/importer/mapreader.ts
@@ -2,17 +2,40 @@ import { Coordinates } from "./coordinates";
 import * as convert from "xml-js";
 import * as fs from 'fs';
 
-const fileName = 'map-small.osm';
+const fileName = 'map';
 const cwd = process.cwd() + "/importer";
 
 export class MapReader {
 	
 
 	async readMap() {
-		const xmlFilePath = cwd + "/map/" + fileName;
 
-		var xmlData = fs.readFileSync(xmlFilePath, 'utf8');
-		var jsonData = convert.xml2json(xmlData, {compact: false, spaces: 4});
+		const xmlFilePath = cwd + "/map/" + fileName + ".osm";
+
+		try {
+			let xmlData = fs.readFileSync(xmlFilePath, 'utf8');
+			let jsonString = convert.xml2json(xmlData, {compact: false, spaces: 4});
+			var jsonData = JSON.parse(jsonString);
+
+			// const filePath = cwd + "/map/" + fileName + ".json";
+			// try {
+			// 	fs.writeFileSync(filePath, jsonString, 'utf-8');
+			// 	console.log(`JSON data has been written to ${filePath}`);
+			// } catch (error) {
+			// 	console.error(`Error writing to file: ${error}`);
+			// }
+
+		} catch (error) {
+			console.error(error);
+		}
+
+		console.log(jsonData.elements[0].elements);
+
+		let nodes = jsonData.elements[0].elements.filter(element => element.name == "node");
+		let ways = jsonData.elements[0].elements.filter(element => element.name == "way");
+		let relations = jsonData.elements[0].elements.filter(element => element.name == "relation");
+
+		console.log(ways);
 
 	}
 
