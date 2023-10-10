651544f21c6af52eb7c6e87542c357e4cd011448
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2023-10-10 16:34:36 +0200

    Add create game

--- page/create.tsx
+++ page/create.tsx
@@ -1,5 +1,49 @@
 import { Component } from "@acryps/page";
 
-export class CreateComponent extends Component {
-	
+export class CreateGameComponent extends Component {
+	declare parameters: {
+		latitude,
+		longitude
+	}
+
+	radii = [0.001, 0.002, 0.005];
+	radius = this.radii[1];
+
+	render() {
+		return <ui-lobby>
+			<ui-location>
+				{this.parameters.latitude} {this.parameters.longitude}
+			</ui-location>
+
+			<ui-ranges>
+				{this.radii.map(radius => <ui-range ui-active={radius == this.radius} ui-click={() => {
+					this.radius = radius;
+
+					this.update();
+				}}>
+					+/- {radius}
+				</ui-range>)}
+			</ui-ranges>
+
+			<ui-action ui-start ui-click={async () => {
+				const token = await fetch('/game', {
+					method: 'post',
+					headers: {
+						'content-type': 'application/json'
+					},
+					body: JSON.stringify({
+						center: {
+							latitude: this.parameters.latitude,
+							longitude: this.parameters.longitude
+						},
+						radius: this.radius
+					})
+				}).then(response => response.json());
+
+				this.navigate(`/play/${token}`);
+			}}>
+				Start Game
+			</ui-action>
+		</ui-lobby>;
+	}
 }
--- page/game/index.tsx
+++ page/game/index.tsx
@@ -1,10 +1,21 @@
 import { Component } from "@acryps/page";
 import { Point } from "./map";
+import { LobbyComponent } from "./lobby";
 
 export class GameComponent extends Component {
+	declare parameters: { token };
+
 	center: Point;
 	radius = 0.1;
 	resolution = 0.0001;
+
+	lobby = new LobbyComponent();
+
+	socket: WebSocket;
+
+	async onload() {
+		this.socket = new WebSocket(`${location.protocol.replace('http', 'ws')}//${location.host}/join/${this.parameters.token}`);
+	}
 
 	render() {
 		const mapCanvas = document.createElement('canvas');
@@ -20,6 +31,8 @@ export class GameComponent extends Component {
 		});
 
 		return <ui-game>
+			{this.lobby}
+
 			{mapCanvas}
 		</ui-game>;
 	}
