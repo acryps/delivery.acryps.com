c4fcd0407b951c6bba61a57cc5f6713bda59d2b7
Eggsecuter <adrian.habeler@gmail.com>
2023-10-10 18:02:35 +0200

    Fix package import

--- server/importer/mapreader.ts
+++ server/importer/mapreader.ts
@@ -1,11 +1,9 @@
 import { Coordinates } from "./coordinates";
-
+import * as convert from "xml-js";
 import * as fs from 'fs';
 
 const fileName = 'map-small.osm';
 const cwd = process.cwd() + "/importer";
-
-var convert = require('xml-js');
 
 export class MapReader {
 	
@@ -13,7 +11,7 @@ export class MapReader {
 	async readMap() {
 		const xmlFilePath = cwd + "/map/" + fileName;
 
-		var xmlData = require('fs').readFileSync(xmlFilePath, 'utf8');
+		var xmlData = fs.readFileSync(xmlFilePath, 'utf8');
 		var jsonData = convert.xml2json(xmlData, {compact: false, spaces: 4});
 
 	}
