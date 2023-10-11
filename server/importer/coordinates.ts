5e8335186d6818d02ed40b843b0b3c3ef6a00d1d
yael <yael.fassbind@acryps.com>
2023-10-11 14:50:33 +0200

    Add coordinates

--- server/importer/coordinates.ts
+++ server/importer/coordinates.ts
@@ -1,3 +1,12 @@
 export class Coordinates {
-	constructor(latitude: number, longitude: number) {}
+	latitude: number;
+	longitude: number;
+	constructor(latitude: number, longitude: number) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	toString(): string {
+		return this.latitude + "," + this.longitude;
+	}
 }
