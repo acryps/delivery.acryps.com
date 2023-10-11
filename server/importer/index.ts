e75bedadf4021ba6261e7069052a49c12a22cf09
yael <yael.fassbind@acryps.com>
2023-10-11 14:51:48 +0200

    WIP: Add get buildings and streets

--- server/importer/index.ts
+++ server/importer/index.ts
@@ -2,7 +2,7 @@ import { DbContext } from "../managed/database";
 import { MapReader } from "./mapreader";
 
 export function importArea(database: DbContext) {
-	let mapreader = new MapReader();
+	let mapreader = new MapReader(database);
 	mapreader.readMap();
 
 }
--- server/importer/mapreader.ts
+++ server/importer/mapreader.ts
@@ -1,108 +1,223 @@
 import { Coordinates } from "./coordinates";
 import * as convert from "xml-js";
 import * as fs from 'fs';
+import { DbContext } from "../managed/database";
 
-const fileName = 'map';
+const fileName = 'oberarth';
 const cwd = process.cwd() + "/importer";
 
 export class MapReader {
+	jsonData;
+	nodes;
+	ways;
+	relations;
 
-	async readMap() {
+	db;
 
+	constructor(db: DbContext) {
+		this.db = db;
+		this.jsonData = this.readMap();
+
+		this.nodes = this.jsonData.osm.node;
+		this.ways = this.jsonData.osm.way;
+		this.relations = this.jsonData.osm.relation;
+	}
+
+	readMap() {
 		const xmlFilePath = cwd + "/map/" + fileName + ".osm";
 
 		try {
 			let xmlData = fs.readFileSync(xmlFilePath, 'utf8');
-			let jsonString = convert.xml2json(xmlData, {compact: false, spaces: 4});
+			let jsonString = convert.xml2json(xmlData, {compact: true, spaces: 4});
 			var jsonData = JSON.parse(jsonString);
-
-			// const filePath = cwd + "/map/" + fileName + ".json";
-			// try {
-			// 	fs.writeFileSync(filePath, jsonString, 'utf-8');
-			// 	console.log(`JSON data has been written to ${filePath}`);
-			// } catch (error) {
-			// 	console.error(`Error writing to file: ${error}`);
-			// }
-
 		} catch (error) {
 			console.error(error);
 		}
 
-		console.log(jsonData.elements[0].elements);
-
-		let nodes = jsonData.elements[0].elements.filter(element => element.name == "node");
-		let ways = jsonData.elements[0].elements.filter(element => element.name == "way");
-		let relations = jsonData.elements[0].elements.filter(element => element.name == "relation");
-
-		console.log(ways);
-
+		return jsonData;
 	}
 
+	getBuildings() {
+		let buildings = this.filterWaysByAttribute("building");
 
-    getHouses() {
-        let buildings: Element[];
-        // get building ways
+		buildings.forEach(building => {
+			let polygonString = this.constructPolygonString(building);
 
-		// get corner points of buildings
-		// convert corner points into polygon string
+			let address = this.extractAddress(building);
 
-		// get address
+			let center = this.calculateCenter(this.getCoordinates(building));
 
-		// calculate center point
+		});
 
-        const xmlDoc = this.readMap();
-
-        const ways = xmlDoc.getElementsByTagName('ways');
-
-        // get xml tags of buildings
-        for (let way of ways) {
-            if(way.getElementsByTagName('tag').length != 0) {
-                if (way.getAttribute('k') == 'building' && way.getAttribute('v') == 'yes') {
-                    buildings.push(way);
-                }
-            }
-        }
-
-        // get corner points of buildings
-        for (let building of buildings) {
-            if (building.getElementsByTagName('nd').length != 0) {
-                const nodes = xmlDoc.getElementsByTagName('nd');
-                
-                for (let node of nodes) {
-                    if (node.hasAttribute('ref')) {
-                        const nodeReference = node.getAttribute('red');
-
-                        this.getNode(nodeReference);
-                    }
-                }
-            }
-        }
-    }
-
-    getNode(nodeReference: string) {
-        const xmlDoc = this.readMap();
-
-        //get node with the node reference as ID to get latitude and longitude
-    }
+		return buildings;
+	}
 
 	getStreets() {
-		// todo: figure out what types of streets there are and bundle them
-		// bridges, highways, paths, streets, mainstreets, ... => streets
-	
+		let highways = this.filterWaysByAttribute("highway");
+		// todo:
 		// get street names
-	
 		// get corner points
-	
 		// calculate center
-	
+
+		return highways;
 	}
+
+
+	getCoordinatesOfNode(id: string): Coordinates {
+		let filteredNodes = this.nodes.filter(element => element._attributes.id === id);
+
+		if(filteredNodes.length > 1) {
+			console.error("didn't get unique element");
+			return null;
+		}
+		if(filteredNodes.length == 0) {
+			console.error("got no elements");
+			return null;
+		}
+
+		let node = filteredNodes[0];	
+		return new Coordinates(+node._attributes.lat, +node._attributes.lon);
+	}
+
+	getNode(id: string) {
+		let node = this.nodes.filter(element => element._attributes.id === id);
+
+		if(node === null) console.error("no node returned");
+
+		return node;
+	}
+
+	/**
+	 * returns the "way" objects which have a tag with the given attribute.
+	 * @param attribute 
+	 * @returns 
+	 */
+	filterWaysByAttribute(attribute: string) {
+		let filtered = [];
+		this.ways.forEach(way => {
+			if(way.tag) {
+				if(way.tag.length > 1) {
+					way.tag.forEach(wayTag => {
+						if(wayTag._attributes.k === attribute) filtered.push(way);
+					});
+				}
+				else {
+					if(way.tag._attributes.k === attribute) filtered.push(way);
+				}
+			}
+		});
+		return filtered;
+	}
+
+	/**
+	 * returns the coordinates, which form the polygon of the given building
+	 * @param building 
+	 * @returns 
+	 */
+	getCoordinates(building): Coordinates[] {
+		let coordinates: Coordinates[] = [];
+		let buildingNodes = building.nd;
+
+		if(buildingNodes) {
+			if(buildingNodes.length > 1) {
+				buildingNodes.forEach(buildingNode => {
+					let nodeRef = buildingNode._attributes.ref;
+					coordinates.push(this.getCoordinatesOfNode(nodeRef));
+				});
+			}
+			else {
+				let nodeRef = buildingNodes._attributes.ref;
+				coordinates.push(this.getCoordinatesOfNode(nodeRef));
+			}
+		}
+
+		return coordinates;
+	}
+
+	/**
+	 * constructs a string containing latitude and longitude which define the polygon of a building.
+	 * the constructed polygonstring has the following format: [latitude], [longitude] ; [latitude], [longitude] ; ...
+	 * @param building 
+	 * @returns 
+	 */
+	constructPolygonString(building): string {
+		let polygonString = "";
+		let buildingNodes = building.nd;
+
+		if(buildingNodes) {
+			if(buildingNodes.length > 1) {
+				buildingNodes.forEach(buildingNode => {
+					let nodeRef = buildingNode._attributes.ref;
+					let coordinates: Coordinates = this.getCoordinatesOfNode(nodeRef);
+					polygonString = coordinates.toString() + ";" + polygonString;
+				});
+			}
+			else {
+				let nodeRef = buildingNodes._attributes.ref;
+				let coordinates: Coordinates = this.getCoordinatesOfNode(nodeRef);
+				polygonString = coordinates.toString() + ";" + polygonString;
+			}
+		}
+		return polygonString;
+	}
+
+	/**
+	 * extracts address of the given building, if the address is defined. otherwise it returns an empty string.
+	 * the address has the following format: [street] [housenumber] [postcode] [city]
+	 * @param building 
+	 * @returns 
+	 */
+	extractAddress(building): string {
+		let buildingTags = building.tag;
+		let city: string = "";
+		let housenumber: string = "";
+		let postcode: string = "";
+		let street: string = "";
+
+		if(buildingTags && buildingTags.length > 1) {
+			buildingTags.forEach(tag => {
+				switch (tag._attributes.k) {
+					case "addr:city":
+						city = tag._attributes.v;
+						break;
+					case "addr:housenumber":
+						housenumber = tag._attributes.v;
+						break;
+					case "addr:postcode":
+						postcode = tag._attributes.v;
+						break;
+					case "addr:street":
+						street = tag._attributes.v;
+						break;
+					default:
+						break;
+				}
+			});
+		}
+		else {
+			console.warn("building has no address");
+		}
+
+		return street + " " + housenumber + " " + postcode + " " + city;
+	} 
 	
+	/**
+	 * calculates the approximate center of the given coordinates
+	 * @param coordinates 
+	 * @returns approximate center
+	 */
 	calculateCenter(coordinates: Coordinates[]): Coordinates {
-		let center: Coordinates = null;
-		// todo: implement center calculations
+		let sumLatitude = 0;
+		let sumLongitude = 0;
+
+		coordinates.forEach(coordinate => {
+			sumLatitude += coordinate.latitude;
+			sumLongitude += coordinate.longitude;
+		});
+
+		let numberOfCoordinates = coordinates.length;
+		let center: Coordinates = new Coordinates(sumLatitude/numberOfCoordinates, sumLongitude/numberOfCoordinates);
 	
 		return center;
 	}
 }
-
-
