885b419ad37efc3957bbb5dd5a155e4a19903749
younesxd <eltaief.younes@hotmail.com>
2023-10-12 09:48:32 +0200

    Add lat long data to database

--- database/2-bounding-boxes.sql
+++ database/2-bounding-boxes.sql
@@ -3,3 +3,11 @@ CREATE TABLE bounding_boxes (
 
     "polygon" TEXT
 );
+
+ALTER TABLE bounding_boxes DROP "polygon";
+
+ALTER TABLE bounding_boxes ADD latitude_max REAL;
+ALTER TABLE bounding_boxes ADD latitude_min REAL;
+
+ALTER TABLE bounding_boxes ADD longitude_max REAL;
+ALTER TABLE bounding_boxes ADD longitude_min REAL;
