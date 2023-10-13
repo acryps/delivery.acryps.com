dd4c5553733423a15eea57bd6a541d69b6d7321c
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2023-10-13 15:56:31 +0200

    Fix point walking

--- page/index.tsx
+++ page/index.tsx
@@ -5,6 +5,7 @@ import { HomeComponent } from './home';
 import { GameComponent } from './game';
 import { JoinComponent } from './join';
 import { CreateGameComponent } from './create';
+import { Point } from '../shared/point';
 
 export class Application {
 	static router: Router;
@@ -21,6 +22,11 @@ export class Application {
 		registerDirectives(Component, this.router);
 
 		this.router.host(document.body);
+
+		const start = new Point(47.38468034073662, 8.53153377949949);
+		const end = start.walk(Math.PI / 3, 1491);
+
+		console.debug(`https://www.google.com/maps/place/${end.latitude},+${end.longitude}/@${end.latitude},${end.longitude}`, start.distance(end), end.distance(start));
 	}
 }
 
