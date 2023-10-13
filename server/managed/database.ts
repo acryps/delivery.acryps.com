d9f29fd67f149879834fbd87de13f27bc5db4a6e
younesxd <eltaief.younes@hotmail.com>
2023-10-13 14:38:22 +0200

    Add importer id and boolean for address to database buildings

--- server/managed/database.ts
+++ server/managed/database.ts
@@ -1,37 +1,5 @@
 import { Entity, DbSet, RunContext, QueryUUID, QueryProxy, QueryString, QueryJSON, QueryTimeStamp, QueryNumber, QueryTime, QueryDate, QueryBoolean, QueryBuffer, QueryEnum, ForeignReference, PrimaryReference, View, ViewSet } from "vlquery";
 
-export class BuildingQueryProxy extends QueryProxy {
-	get centerlatitude(): Partial<QueryNumber> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
-	get centerlongitude(): Partial<QueryNumber> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
-	get polygon(): Partial<QueryString> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
-	get address(): Partial<QueryString> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
-}
-
-export class Building extends Entity<BuildingQueryProxy> {
-	declare id: string;
-	centerlatitude: number;
-	centerlongitude: number;
-	polygon: string;
-	address: string;
-	
-
-	$$meta = {
-		source: "building",
-
-		columns: {
-			id: { type: "uuid", name: "id" },
-			centerlatitude: { type: "float4", name: "centerlatitude" },
-			centerlongitude: { type: "float4", name: "centerlongitude" },
-			polygon: { type: "text", name: "polygon" },
-			address: { type: "text", name: "address" }
-		},
-
-		get set(): DbSet<Building, BuildingQueryProxy> { 
-			return new DbSet<Building, BuildingQueryProxy>(Building, null);
-		}
-	};
-}
-			
 export class StreetQueryProxy extends QueryProxy {
 	get centerlatitude(): Partial<QueryNumber> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
 	get centerlongitude(): Partial<QueryNumber> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
@@ -96,23 +64,63 @@ export class WaterBody extends Entity<WaterBodyQueryProxy> {
 	};
 }
 			
+export class BuildingQueryProxy extends QueryProxy {
+	get centerlatitude(): Partial<QueryNumber> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
+	get centerlongitude(): Partial<QueryNumber> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
+	get addressReal(): Partial<QueryBoolean> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
+	get polygon(): Partial<QueryString> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
+	get address(): Partial<QueryString> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
+	get importerId(): Partial<QueryString> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
+}
+
+export class Building extends Entity<BuildingQueryProxy> {
+	centerlatitude: number;
+	centerlongitude: number;
+	addressReal: boolean;
+	declare id: string;
+	polygon: string;
+	address: string;
+	importerId: string;
+	
+
+	$$meta = {
+		source: "building",
+
+		columns: {
+			centerlatitude: { type: "float4", name: "centerlatitude" },
+			centerlongitude: { type: "float4", name: "centerlongitude" },
+			addressReal: { type: "bool", name: "address_real" },
+			id: { type: "uuid", name: "id" },
+			polygon: { type: "text", name: "polygon" },
+			address: { type: "text", name: "address" },
+			importerId: { type: "text", name: "importer_id" }
+		},
+
+		get set(): DbSet<Building, BuildingQueryProxy> { 
+			return new DbSet<Building, BuildingQueryProxy>(Building, null);
+		}
+	};
+}
+			
 export class ImportQueryProxy extends QueryProxy {
 	get created(): Partial<QueryTimeStamp> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
-	get center(): Partial<QueryNumber> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
+	get centerLatitude(): Partial<QueryNumber> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
 	get minLatitude(): Partial<QueryNumber> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
 	get maxLatitude(): Partial<QueryNumber> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
 	get minLongitude(): Partial<QueryNumber> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
 	get maxLongitude(): Partial<QueryNumber> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
+	get centerLongitude(): Partial<QueryNumber> { throw new Error("Invalid use of QueryModels. QueryModels cannot be used during runtime"); }
 }
 
 export class Import extends Entity<ImportQueryProxy> {
 	declare id: string;
 	created: Date;
-	center: number;
+	centerLatitude: number;
 	minLatitude: number;
 	maxLatitude: number;
 	minLongitude: number;
 	maxLongitude: number;
+	centerLongitude: number;
 	
 
 	$$meta = {
@@ -121,11 +129,12 @@ export class Import extends Entity<ImportQueryProxy> {
 		columns: {
 			id: { type: "uuid", name: "id" },
 			created: { type: "timestamp", name: "created" },
-			center: { type: "float4", name: "center" },
+			centerLatitude: { type: "float4", name: "center_latitude" },
 			minLatitude: { type: "float4", name: "min_latitude" },
 			maxLatitude: { type: "float4", name: "max_latitude" },
 			minLongitude: { type: "float4", name: "min_longitude" },
-			maxLongitude: { type: "float4", name: "max_longitude" }
+			maxLongitude: { type: "float4", name: "max_longitude" },
+			centerLongitude: { type: "float4", name: "center_longitude" }
 		},
 
 		get set(): DbSet<Import, ImportQueryProxy> { 
@@ -136,15 +145,15 @@ export class Import extends Entity<ImportQueryProxy> {
 			
 
 export class DbContext {
-	building: DbSet<Building, BuildingQueryProxy>;
 	street: DbSet<Street, StreetQueryProxy>;
 	waterBody: DbSet<WaterBody, WaterBodyQueryProxy>;
+	building: DbSet<Building, BuildingQueryProxy>;
 	import: DbSet<Import, ImportQueryProxy>;
 
 	constructor(private runContext: RunContext) {
-		this.building = new DbSet<Building, BuildingQueryProxy>(Building, this.runContext);
 		this.street = new DbSet<Street, StreetQueryProxy>(Street, this.runContext);
 		this.waterBody = new DbSet<WaterBody, WaterBodyQueryProxy>(WaterBody, this.runContext);
+		this.building = new DbSet<Building, BuildingQueryProxy>(Building, this.runContext);
 		this.import = new DbSet<Import, ImportQueryProxy>(Import, this.runContext);
 	}
 
