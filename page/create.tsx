580b8092d00fef6ae54774a7c77ff54d9f0b81cf
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2023-10-24 11:16:44 +0200

    Add start page design

--- page/create.tsx
+++ page/create.tsx
@@ -1,11 +1,17 @@
 import { Component } from "@acryps/page";
 import { gameConfiguration } from "../shared/constants";
+import { Point } from "../shared/point";
 
 export class CreateGameComponent extends Component {
-	declare parameters: {
-		latitude,
-		longitude
-	}
+	position = new Point(47.3731429, 8.5239003);
+
+	positionPresets = [
+		new LocationPreset('Acryps Zürich', new Point(47.3731429, 8.5239003)),
+		new LocationPreset('Acryps Luzern', new Point(47.04981, 8.30086)),
+		new LocationPreset('Niederdorf', new Point(47.37184, 8.54381)),
+		new LocationPreset('London Marylebone', new Point(51.51901, 0.15836)),
+		new LocationPreset('Manhattan', new Point(40.72631, 73.99515))
+	];
 
 	radius = gameConfiguration.radii[gameConfiguration.defaultRadiusIndex];
 	duration = gameConfiguration.durationMinutes[gameConfiguration.defaultDurationIndex];
@@ -14,39 +20,68 @@ export class CreateGameComponent extends Component {
 		return <ui-create-game>
 			<ui-location>
 				<ui-title>
-					Location
+					Ort
 				</ui-title>
 
-				{this.parameters.latitude} {this.parameters.longitude}
+				<ui-form>
+					<input type='number' $ui-value={this.position.latitude} />
+					<input type='number' $ui-value={this.position.longitude} />
+				</ui-form>
+
+				<ui-presets>
+					<ui-action ui-current-location ui-click={async () => {
+						const position = await this.getCurrentLocation();
+
+						if (position) {
+							this.position = position;
+
+							this.update();
+						}
+					}}>
+						Dein Standort
+					</ui-action>
+
+					{this.positionPresets.map(preset => <ui-action ui-preset ui-click={() => {
+						this.position = preset.location;
+
+						this.update();
+					}}>
+						{preset.name}
+					</ui-action>)}
+				</ui-presets>
 			</ui-location>
 
-			<ui-radii>
+			<ui-radius>
 				<ui-title>
-					Radius
+					Spielfeld
 				</ui-title>
 
-				{gameConfiguration.radii.map(radius => <ui-radius ui-active={radius == this.radius} ui-click={() => {
-					this.radius = radius;
+				<ui-radii>
+					{gameConfiguration.radii.map(radius => <ui-radius ui-active={radius == this.radius} ui-click={() => {
+						this.radius = radius;
 
-					this.update();
-				}}>
-					+/- {radius}
-				</ui-radius>)}
-			</ui-radii>
+						this.update();
+					}}>
+						{radius * 2}m
+					</ui-radius>)}
+				</ui-radii>
+			</ui-radius>
 
-			<ui-durations>
+			<ui-duration>
 				<ui-title>
-					Duration
+					Spielzeit
 				</ui-title>
 
-				{gameConfiguration.durationMinutes.map(duration => <ui-duration ui-active={duration == this.duration} ui-click={() => {
-					this.duration = duration;
+				<ui-durations>
+					{gameConfiguration.durationMinutes.map(duration => <ui-duration ui-active={duration == this.duration} ui-click={() => {
+						this.duration = duration;
 
-					this.update();
-				}}>
-					{duration}'
-				</ui-duration>)}
-			</ui-durations>
+						this.update();
+					}}>
+						{duration}'
+					</ui-duration>)}
+				</ui-durations>
+			</ui-duration>
 
 			<ui-action ui-create ui-click-text='Preparing Game...' ui-click={async () => {
 				const token = await fetch('/game', {
@@ -68,8 +103,33 @@ export class CreateGameComponent extends Component {
 			}}>
 				Create Game
 			</ui-action>
-
-			<ui-action ui-back ui-click={() => this.navigate('/')}>Back</ui-action>
 		</ui-create-game>;
 	}
+
+	async getCurrentLocation() {
+		if ('permissions' in navigator) {
+			const permission = await navigator.permissions.query({ name: 'geolocation' });
+
+			if (permission.state != 'granted') {
+				return;
+			}
+		}
+
+		if ('geolocation' in navigator) {
+			return new Promise<Point>(done => {
+				navigator.geolocation.getCurrentPosition(position => {
+					done(new Point(position.coords.latitude, position.coords.longitude));
+				}, () => {
+					done(null);
+				});
+			});
+		}
+	}
+}
+
+export class LocationPreset {
+	constructor(
+		public name: string,
+		public location: Point
+	) {}
 }
---
+++ page/home/index.tsx
@@ -0,0 +1,47 @@
+import { Component } from "@acryps/page";
+import { Point } from "../../shared/point";
+import { tokenLength } from "../../shared/constants";
+import { CreateGameComponent } from "../create";
+
+export class HomeComponent extends Component {
+	private token = '';
+	private invalidToken = false;
+
+	async join() {
+		const joinableGame = await fetch(`/game/${this.token}`).then(response => response.json());
+
+		if (joinableGame) {
+			this.navigate(`/play/${this.token}`);
+		} else {
+			this.invalidToken = true;
+
+			this.update();
+		}
+	}
+
+	render() {
+		return <ui-home>
+			<ui-section>
+				<ui-title>
+					Spiel Beitreten
+				</ui-title>
+
+				<ui-join>
+					<input $ui-value={this.token} maxlength={tokenLength} placeholder='Code' ui-error={this.invalidToken} />
+
+					<ui-action ui-join ui-click-text='Joining...' ui-click={async () => this.token.length && this.join()}>
+						Beitreten
+					</ui-action>
+				</ui-join>
+			</ui-section>
+
+			<ui-panel>
+				<ui-title>
+					Spiel Erstellen
+				</ui-title>
+
+				{new CreateGameComponent()}
+			</ui-panel>
+		</ui-home>
+	}
+}
